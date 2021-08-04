bf8d320469afe7b073823fd36caa23689b79d87b
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2021-08-04 16:09:43 +0200

    add m-bus package
    Add package for Modbus TCP/RTU stack.

---
+++ package/m-bus/m-bus.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# m-bus
+#
+################################################################################
+
+M_BUS_VERSION = c8193517d020fa1e701bbab4ee0220ef6e90445d
+M_BUS_SITE = git://github.com/rtlabs-com/m-bus.git
+M_BUS_GIT_SUBMODULES = YES
+M_BUS_INSTALL_STAGING = YES
+M_BUS_LICENSE = Dual-licensed under GPLv3 or a commercial license
+M_BUS_LICENSE_FILES = LICENSE.md
+M_BUS_SUPPORTS_IN_SOURCE_BUILD = NO
+M_BUS_CONF_OPTS += \
+	-DUSE_SCHED_FIFO=$(if $(BR2_PACKAGE_M_BUS_USE_SCHED_FIFO),ON,OFF)
+
+$(eval $(cmake-package))
