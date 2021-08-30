d87a567e093677f4ac03e84ace6761c9d5326a29
MechaMagpie <emma.grampp@rt-labs.com>
2021-08-30 17:04:42 +0200

    Added SOES Etherberry demo switch
    Added an option in the SOES package to set the "RPI_ETHERBERRY_DEMO" compile
    flag, which will make SOES compile a demo for aforementioned dev board.

--- package/soes/soes.mk
+++ package/soes/soes.mk
@@ -10,10 +10,16 @@ SOES_INSTALL_STAGING = YES
 SOES_LICENSE = Licensed under the GNU General Public License version 2 with exceptions
 SOES_LICENSE_FILES = LICENSE
 SOES_SUPPORTS_IN_SOURCE_BUILD = NO
-SOES_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
+SOES_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+SOES_CONF_OPTS += \
+	-DRPI_VARIANT=$(if $(BR2_PACKAGE_SOES_RPI_ETHERBERRY_DEMO),ON,OFF)
 
 SOES_MODULE_SUBDIRS = drivers/linux/lan9252
 SOES_MODULE_MAKE_OPTS = CONFIG_LAN9252=m CFLAGS_lan9252.o=-DDEBUG
+
+define SOES_PERMISSIONS
+	/etc/init.d/S60soes f 111 root root - - - - -
+endef
 
 $(eval $(kernel-module))
 $(eval $(cmake-package))
